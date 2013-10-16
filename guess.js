c7e09f36235a24be97bcc8e65a2abde5e4e46632
Peter Parente <parente@cs.unc.edu>
2013-10-16 23:52:25 +0000

    Harden, ignore mods

--- guess.js
+++ guess.js
@@ -1,22 +1,16 @@
+// quick guessing game
 var readline = require('readline'),
     rl = readline.createInterface(process.stdin, process.stdout);
 
 var max = 10;
-var prompt = 'Guess a number between 1 and ' + max + ': ';
 var left = 3;
+var prompt = 'Guess a number between 1 and ' + max + ': ';
 var target = Math.floor(Math.random() * max) + 1;
 
 rl.setPrompt('(' + left + ') ' + prompt);
 rl.prompt();
-
 rl.on('line', function(line) {
-    var guess;
-    try {
-        guess = Number(line);
-    } catch(e) {
-        rl.prompt();
-        return;
-    }
+    var guess = Number(line);
 
     if(guess === target) {
         console.log('You got it!');
@@ -28,6 +22,10 @@ rl.on('line', function(line) {
         console.log('Higher.');
     } else if(guess > target) {
         console.log('Lower.');
+    }
+
+    if(isNaN(guess)) {
+        console.log('Uh ... upwards?');
     }
 
     rl.setPrompt('(' + left + ') ' + prompt);
